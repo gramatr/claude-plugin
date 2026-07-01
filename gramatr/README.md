651e648ec4e1f7d11634c716862cb59991230229
gramatr-ci <ci@gramatr.com>
2026-07-01 04:35:00 +0000

    release: gramatr claude-plugin v0.27.1

--- gramatr/README.md
+++ gramatr/README.md
@@ -33,4 +33,4 @@ https://gramatr.com
 
 ## Version
 
-0.27.0
+0.27.1
--- gramatr/bin/gramatr-proxy.js
+++ gramatr/bin/gramatr-proxy.js
@@ -664,8 +664,8 @@ async function mintProxyTokenFromKeyring(dataDir, baseUrl, fetchImpl = fetch, no
 var import_meta = {};
 function resolveProxyVersion() {
   try {
-    if ("0.27.0") {
-      return "0.27.0";
+    if ("0.27.1") {
+      return "0.27.1";
     }
   } catch {
   }
