cdd376fd18c2ea8d8e1dccd6c1b35b5b58675226
gramatr-ci <ci@gramatr.com>
2026-08-06 04:43:16 +0000

    release: gramatr claude-plugin v0.30.4

--- gramatr/README.md
+++ gramatr/README.md
@@ -33,4 +33,4 @@ https://gramatr.com
 
 ## Version
 
-0.30.3
+0.30.4
--- gramatr/bin/gramatr-proxy.js
+++ gramatr/bin/gramatr-proxy.js
@@ -664,8 +664,8 @@ async function mintProxyTokenFromKeyring(dataDir, baseUrl, fetchImpl = fetch, no
 var import_meta = {};
 function resolveProxyVersion() {
   try {
-    if ("0.30.3") {
-      return "0.30.3";
+    if ("0.30.4") {
+      return "0.30.4";
     }
   } catch {
   }
