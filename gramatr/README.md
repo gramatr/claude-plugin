58a83e19524eb3b2448b5144173070826da8327d
gramatr-ci <ci@gramatr.com>
2026-08-10 21:55:33 +0000

    release: gramatr claude-plugin v0.32.10

--- gramatr/README.md
+++ gramatr/README.md
@@ -33,4 +33,4 @@ https://gramatr.com
 
 ## Version
 
-0.32.9
+0.32.10
--- gramatr/bin/gramatr-proxy.js
+++ gramatr/bin/gramatr-proxy.js
@@ -664,8 +664,8 @@ async function mintProxyTokenFromKeyring(dataDir, baseUrl, fetchImpl = fetch, no
 var import_meta = {};
 function resolveProxyVersion() {
   try {
-    if ("0.32.9") {
-      return "0.32.9";
+    if ("0.32.10") {
+      return "0.32.10";
     }
   } catch {
   }
