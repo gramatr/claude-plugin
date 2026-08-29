14f90220a29845de643c40a4467cc74f68bb97be
bhandrigan <brian@gramatr.com>
2026-08-29 18:09:48 +0000

    release: gramatr claude-plugin v0.35.4

--- gramatr/README.md
+++ gramatr/README.md
@@ -33,4 +33,4 @@ https://gramatr.com
 
 ## Version
 
-0.35.3
+0.35.4
--- gramatr/bin/gramatr-proxy.js
+++ gramatr/bin/gramatr-proxy.js
@@ -991,8 +991,8 @@ async function mintProxyTokenFromKeyring(dataDir, baseUrl, fetchImpl = fetch, no
 var import_meta = {};
 function resolveProxyVersion() {
   try {
-    if ("0.35.3") {
-      return "0.35.3";
+    if ("0.35.4") {
+      return "0.35.4";
     }
   } catch {
   }
